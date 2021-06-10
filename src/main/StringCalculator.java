cb0195e4f8ea1739171a370d7d5fb0ad69a2c857
fernando-nunes21 <luis@ubots.com.br>
2021-06-10 03:38:22 -0300

    -- TEST GREEN --

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -1,15 +1,37 @@
 package main;
 
+import java.util.ArrayList;
+
 public class StringCalculator implements StringCalculatorIntarface{
+    private ArrayList<String> numbers = new ArrayList<>();
+
     public String add(String numbers){
-        if(numbers.equals("2")){
-            return "2";
+        String[] separeNumbers = numbers.split(",");
+        for(int i=0;i<separeNumbers.length;i++){
+            this.numbers.add(separeNumbers[i]);
         }
-        else if(numbers.equals("")){
+
+        return sum();
+    }
+
+    private String sum(){
+        Double sum = 0.0;
+        String result="";
+
+        if(this.numbers.size() == 1 && this.numbers.get(0) == ""){
             return "0";
         }
-        else {
-            return null;
+        else{
+            for (int i=0;i<this.numbers.size();i++){
+                sum+= Double.parseDouble(this.numbers.get(i));
+            }
+            result = sum.toString();
+            if(result.length()<4 && result.contains(".0")){
+                Integer convert = sum.intValue();
+                result = convert.toString();
+            }
         }
+        return result;
     }
+
 }
